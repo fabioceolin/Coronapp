4665ea246b11bc56f95abe9e21d58ed12cc42eac
fabioceolin <fabioceolin98@gmail.com>
2020-03-24 22:40:06 -0300

    Add chartScript

--- src/js/app.js
+++ src/js/app.js
@@ -15,6 +15,8 @@ import routes from './routes.js';
 // Import main app component
 import App from '../app.f7.html';
 
+import Script from './chartscript'
+
 var app = new Framework7({
   root: '#app', // App root element
   component: App, // App main component
@@ -46,25 +48,4 @@ var app = new Framework7({
       }
     },
   },
-});
-
-
-import Chart from 'chart.js';
-
-$$(document).on('page:init', '.page[data-name="barchart"]', function (e) {
-    var ctx = $$("#myChart");
-    var myChart = new Chart(ctx, {
-        type: 'line',
-
-        data: {
-            labels: ['2000', '2001', '2002', '2003', '2004', '2005'],
-
-            datasets: [
-                {
-                    label: 'Crecimento Populacional',
-                    data: [173448346, 175885229, 178276128, 180619108, 182911487, 185150806]
-                }
-            ]
-        }
-    });
 });
--- src/js/chartscript.js
+++ src/js/chartscript.js
@@ -1,2 +1,21 @@
 import $$ from 'dom7';
 
+import Chart from 'chart.js';
+
+$$(document).on('page:init', '.page[data-name="barchart"]', function (e) {
+    var ctx = $$("#myChart");
+    var myChart = new Chart(ctx, {
+        type: 'line',
+
+        data: {
+            labels: ['2000', '2001', '2002', '2003', '2004', '2005'],
+
+            datasets: [
+                {
+                    label: 'Crecimento Populacional',
+                    data: [173448346, 175885229, 178276128, 180619108, 182911487, 185150806]
+                }
+            ]
+        }
+    });
+});
