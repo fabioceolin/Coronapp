3bebe6623729402e5dbac10ba9d6e9bee5f01529
Mateus Moura Santos <msantos.96@outlook.com>
2020-03-25 01:05:23 -0300

    busca por pais adicionada na controller

--- src/js/apicontroller.js
+++ src/js/apicontroller.js
@@ -1,8 +1,10 @@
 import $$ from 'dom7'
 import Framework7 from 'framework7/framework7.esm.bundle.js';
-    $$(document).on('page:init', getCountriesList)
+    $$(document).on('page:init', getCountrySummary)//se n chamar c o () vai retornar a instancia da funcao, 
+    //aqui to chamando assim pq o on vai invokar a instnacia da funcao
 
-function getWorldData(){
+
+    function getWorldData(){
     Framework7.request.get('https://api.covid19api.com/summary',null, function (data) {
         var todos = JSON.parse(data)
         let deaths = 0
@@ -14,6 +16,7 @@ function getWorldData(){
             cases = cases + element.TotalConfirmed
         });
         console.log('casos: '+cases+' mortes: '+deaths+ ' curados: '+recovereds)
+        return {confirmed:cases, deaths:deaths, recovereds:recovereds}
     })
 }
 
@@ -27,4 +30,29 @@ function getCountriesList(){
         console.log(paises)
     })
 }
-    
+
+function getCountrySummary(country){
+    if(!(typeof country === 'string')){
+        country = 'brazil'
+    }
+    let baseUrl = 'https://api.covid19api.com/country/'+country+'/status/'
+    let confirmados = 0
+    let mortos = 0
+    let curados = 0
+    Framework7.request.get(baseUrl+'confirmed',null, function(data){
+        let todososconfirmados = JSON.parse(data)
+        confirmados = todososconfirmados[todososconfirmados.length -1].Cases;
+    })
+    Framework7.request.get(baseUrl+'recovered',null, function(data){
+        let todososcurados = JSON.parse(data)
+        curados = todososcurados[todososcurados.length - 1].Cases;
+    })
+    Framework7.request.get(baseUrl+'deaths',null, function(data){
+        let todososmortos = JSON.parse(data)
+        mortos = todososmortos[todososmortos.length -1].Cases;
+        var res = {confirmados:confirmados, mortos:mortos, curados:curados}
+        console.log(res)
+        return res
+    })
+
+}
