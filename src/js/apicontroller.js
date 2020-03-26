e349d1ede6b294f33fff5c07f083ae5722bd895f
Mateus Moura Santos <msantos.96@outlook.com>
2020-03-25 22:16:31 -0300

    timestamp do server adicionado nas requests

--- src/js/apicontroller.js
+++ src/js/apicontroller.js
@@ -1,5 +1,8 @@
 import $$ from 'dom7'
 import Framework7 from 'framework7/framework7.esm.bundle.js';
+
+var paises;
+
     $$(document).on('page:init', getCountrySummary)//se n chamar c o () vai retornar a instancia da funcao, 
     //aqui to chamando assim pq o on vai invokar a instnacia da funcao
 
@@ -15,8 +18,8 @@ import Framework7 from 'framework7/framework7.esm.bundle.js';
             recovereds = recovereds + element.TotalRecovered
             cases = cases + element.TotalConfirmed
         });
-        console.log('casos: '+cases+' mortes: '+deaths+ ' curados: '+recovereds)
-        return {confirmed:cases, deaths:deaths, recovereds:recovereds}
+        console.log('casos: '+cases+' mortes: '+deaths+ ' curados: '+recovereds, ' ts: ' + todos.Date)
+        return {confirmed:cases, deaths:deaths, recovereds:recovereds, date:todos.Date}
     })
 }
 
@@ -27,7 +30,8 @@ function getCountriesList(){
         todos.forEach(element=>{
             paises.push({description:element.Country, id:element.Slug})
         })
-        console.log(paises)
+        console.log({paises:paises})
+        return paises
     })
 }
 
@@ -50,7 +54,7 @@ function getCountrySummary(country){
     Framework7.request.get(baseUrl+'deaths',null, function(data){
         let todososmortos = JSON.parse(data)
         mortos = todososmortos[todososmortos.length -1].Cases;
-        var res = {confirmados:confirmados, mortos:mortos, curados:curados}
+        var res = {confirmados:confirmados, mortos:mortos, curados:curados, date:todososmortos[todososmortos.length -1].Date}
         console.log(res)
         return res
     })
