bf905c82ad5af2e128147bf98ffb779f90ad6510
fabioceolin <fabioceolin98@gmail.com>
2020-03-30 20:00:51 -0300

    Navigation

--- src/js/routes.js
+++ src/js/routes.js
@@ -7,12 +7,10 @@ import FormPage from '../pages/form.f7.html';
 import LoginPage from '../pages/login.f7.html';
 import ChartPage from '../pages/charts.f7.html';
 import CountryListPage from '../pages/countryList.f7.html'
-import test from '../pages/test.f7.html'
 import summaryPage from '../pages/summary.f7.html'
 import timelinePage from '../pages/timeline.f7.html'
 
 import DynamicRoutePage from '../pages/dynamic-route.f7.html';
-import RequestAndLoad from '../pages/request-and-load.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
 
 var routes = [
@@ -45,68 +43,13 @@ var routes = [
     component: summaryPage,
   },
   {
-    path: '/timeline/',
+    path: '/timeline/country/:countrySlug/',
     component: timelinePage,
   },
-  {
-    path: '/test/',
-    component: test,
-  },
-
 
   {
     path: '/dynamic-route/blog/:blogId/post/:postId/',
     component: DynamicRoutePage,
-  },
-  {
-    path: '/request-and-load/user/:userId/',
-    async: function (routeTo, routeFrom, resolve, reject) {
-      // Router instance
-      var router = this;
-
-      // App instance
-      var app = router.app;
-
-      // Show Preloader
-      app.preloader.show();
-
-      // User ID from request
-      var userId = routeTo.params.userId;
-
-      // Simulate Ajax Request
-      setTimeout(function () {
-        // We got user data from request
-        var user = {
-          firstName: 'Vladimir',
-          lastName: 'Kharlampidi',
-          about: 'Hello, i am creator of Framework7! Hope you like it!',
-          links: [
-            {
-              title: 'Framework7 Website',
-              url: 'http://framework7.io',
-            },
-            {
-              title: 'Framework7 Forum',
-              url: 'http://forum.framework7.io',
-            },
-          ]
-        };
-        // Hide Preloader
-        app.preloader.hide();
-
-        // Resolve route to load page
-        resolve(
-          {
-            component: RequestAndLoad,
-          },
-          {
-            context: {
-              user: user,
-            }
-          }
-        );
-      }, 1000);
-    },
   },
   {
     path: '(.*)',
