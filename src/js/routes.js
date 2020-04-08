cf181914788ec74c3ff601d6522b54388f1fa555
fabioceolin <fabioceolin98@gmail.com>
2020-04-08 13:36:06 -0300

    removing unused pages

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,7 +1,7 @@
 
 import HomePage from '../pages/home.f7.html';
 import AboutPage from '../pages/about.f7.html';
-import FormPage from '../pages/form.f7.html';
+// import FormPage from '../pages/form.f7.html';
 
 // Custom Pages
 import LoginPage from '../pages/login.f7.html';
@@ -10,7 +10,7 @@ import CountryListPage from '../pages/countryList.f7.html'
 import summaryPage from '../pages/summary.f7.html'
 import timelinePage from '../pages/timeline.f7.html'
 
-import DynamicRoutePage from '../pages/dynamic-route.f7.html';
+// import DynamicRoutePage from '../pages/dynamic-route.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
 
 var routes = [
@@ -21,10 +21,6 @@ var routes = [
   {
     path: '/about/',
     component: AboutPage,
-  },
-  {
-    path: '/form/',
-    component: FormPage,
   },
   {
     path: '/login/',
@@ -46,11 +42,10 @@ var routes = [
     path: '/timeline/country/:countrySlug/countryName/:countryName',
     component: timelinePage,
   },
-
-  {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    component: DynamicRoutePage,
-  },
+  // {
+  //   path: '/dynamic-route/blog/:blogId/post/:postId/',
+  //   component: DynamicRoutePage,
+  // },
   {
     path: '(.*)',
     component: NotFoundPage,
